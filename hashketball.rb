9270a3ec58fc9006a0c8eb4746288490f6fd5b24
Jason Melton <jason.melton2@gmail.com>
2020-01-29 21:37:03 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -132,7 +132,7 @@ end
 
 
 def num_points_scored(player)
-  points = game_hash.reduce(nil) do |memo, (key, value) 
+  points = game_hash.reduce(nil) do |memo, (key, value)|
   i = 0 
   while i < key[:players].length do 
     if key[:players][i][:player_name].include?(player)
