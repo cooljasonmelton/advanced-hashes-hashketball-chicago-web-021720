c07e861fb458a2baec3492f8e7c73aaf6e14d306
Jason Melton <jason.melton2@gmail.com>
2020-01-29 22:41:35 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -164,6 +164,8 @@ def shoe_size(player)
 end 
 
 def team_colors(team)
+  puts game_hash[:home][:team_name]
+
   if team == game_hash[:home][:team_name]
     colors = game_hash[:home][:team_colors]
   else 
