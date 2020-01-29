dfd392af3f82182a0a6176eca4474d367533cfd8
Jason Melton <jason.melton2@gmail.com>
2020-01-29 23:11:48 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -203,7 +203,8 @@ def player_stats(player)
   while i < just_the_players.length do 
     if player == just_the_players[i][:player_name]
       
-      stats = just_the_players[i].delete(:player_name)
+      stats = just_the_players[i].slice(:player_name)
+      
 
     end 
     i += 1 
