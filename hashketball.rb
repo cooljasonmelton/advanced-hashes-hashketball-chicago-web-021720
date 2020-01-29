cc30a1c38f614b900060ef402cbd07d7238c5c0b
Jason Melton <jason.melton2@gmail.com>
2020-01-29 23:00:25 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -200,7 +200,7 @@ end
 
 def player_stats(player) 
   a = just_the_players.find_index("#{player}")
-  just_the_players[a]
+  puts just_the_players[a]
   
   #i = 0 
   #while i < just_the_players.length do 
